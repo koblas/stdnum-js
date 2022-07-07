307ba47c2ded04741f57135a45121dcbb3c35d08
Mike Ryan <mike.ryan@fountain.com>
2022-07-07 11:33:02 -0400

    Remove license text, the implementation in question was not used as a reference

--- src/be/nn.ts
+++ src/be/nn.ts
@@ -1,26 +1,6 @@
-// This is adapted from work by Cédric Krier Copyright (C) 2021-2022
+// The Belgian national number is a unique identifier consisting of 11 digits.
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-// 02110-1301 USA
-//
-// NN, NISS (Belgian national number).
-//
-// The national number is a unique identifier of Belgian. The number consists of
-// 11 digits.
-//
-// More information:
+// For more information:
 // https://fr.wikipedia.org/wiki/Numéro_de_registre_national
 
 import * as exceptions from '../exceptions';
