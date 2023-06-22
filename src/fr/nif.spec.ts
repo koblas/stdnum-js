78663e70fbaa4c64b2cb04315bfda9f4d5818cbb
David Koblas <david@koblas.com>
2023-06-22 07:16:30 -0400

    fix: checksum calculation for fr/nif

--- src/fr/nif.spec.ts
+++ src/fr/nif.spec.ts
@@ -8,10 +8,16 @@ describe('fr/nif', () => {
     expect(result).toEqual('07 01 987 765 432');
   });
 
-  it('validate:0701987765432', () => {
-    const result = validate('0701987765432');
+  it('validate:3023217600053', () => {
+    const result = validate('302 321 7600 053');
 
-    expect(result.isValid && result.compact).toEqual('0701987765432');
+    expect(result.isValid && result.compact).toEqual('3023217600053');
+  });
+
+  it('validate:0701987765432', () => {
+    const result = validate('0701987765493');
+
+    expect(result.isValid && result.compact).toEqual('0701987765493');
   });
 
   it('validate:12345678', () => {
