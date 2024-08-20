615d5295d0c13d07fa6f53c7cbc76f3d870ec3c8
Alfonso Strotgen <astrotgen@justworks.com>
2024-08-20 09:18:46 -0600

    Follow the new prefix rules.

--- src/gb/nino.ts
+++ src/gb/nino.ts
@@ -2,11 +2,12 @@
  * The National Insurance Number (NINO) is a nine character identifier used in
  * the United Kingdom.
  *
- * The first two characters are from an approved list of prefixes, the next
- * six characters are numbers issued sequentially and the final character is
+ * The first two characters are all valid except for a few rules described in the source document,
+ * the next six characters are numbers issued sequentially, and the final character is
  * a letter A-D. The suffix may be omitted if it is not known.
  *
  * Source
+ *  https://www.gov.uk/hmrc-internal-manuals/national-insurance-manual/nim39110
  *  https://en.wikipedia.org/wiki/National_Insurance_number
  *
  * PERSON
@@ -15,7 +16,6 @@
 import * as exceptions from '../exceptions';
 import { strings } from '../util';
 import { Validator, ValidateReturn } from '../types';
-import PREFIXES from './nino-prefixes';
 
 function clean(input: string): ReturnType<typeof strings.cleanUnicode> {
   return strings.cleanUnicode(input, ' -.');
@@ -26,11 +26,28 @@ function validLength(value: string): boolean {
   return [8, 9].includes(value.length);
 }
 
+function isValidPrefix(prefix: string): boolean {
+  const invalidChars = ["D", "F", "I", "Q", "U", "V"];
+  const invalidSecondChar = "O";
+  const invalidPrefixes = ["BG", "GB", "KN", "NK", "NT", "TN", "ZZ"];
+
+  if (invalidChars.includes(prefix[0]) || invalidChars.includes(prefix[1])) {
+    return false;
+  }
+
+  if (prefix[1] === invalidSecondChar) {
+    return false;
+  }
+
+  return !invalidPrefixes.includes(prefix);
+}
+
 const VALID_FORMAT_REGEX = /^([A-Z]{2})\d{6}[A-D]?$/;
 
 function validFormat(value: string): boolean {
   const matchData = value.toUpperCase().match(VALID_FORMAT_REGEX);
-  return !!matchData && PREFIXES.has(matchData[1]);
+  const prefix = value.slice(0, 2).toUpperCase();
+  return !!matchData && isValidPrefix(prefix);
 }
 
 const impl: Validator = {
