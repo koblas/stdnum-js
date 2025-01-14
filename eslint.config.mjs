85f356d484eb5036ab7fac94a3929d8e5c5c1b51
David Koblas <david@koblas.com>
2025-01-14 05:25:00 -0500

    fix: update packages and linting

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslintConfigPrettier,
+  {
+    ignores: ['lib/*', '.eslintrc.js', 'release.config.js'],
+  },
+);
