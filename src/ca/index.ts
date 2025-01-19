e29f25cea239fee3f1159545c54af681506da141
David Koblas <david@koblas.com>
2025-01-19 06:00:20 -0500

    feat: update to stdnum 20

--- src/ca/index.ts
+++ src/ca/index.ts
@@ -3,3 +3,4 @@ export * as sin from './sin';
 export * as gst from './gst';
 export * as qst from './qst';
 export * as pst from './pst';
+export * as bc_bhn from './bc_bhn';
--- src/in/index.ts
+++ src/in/index.ts
@@ -1,4 +1,5 @@
 export * as aadhaar from './aadhaar';
 export * as pan from './pan';
+export * as vid from './vid';
 export * as epic from './epic';
 export * as gstin from './gstin';
